2d2c624cb690ecdae8e7504700ff2bfb32be3fdb
alpha2phi <alpha2phi@gmail.com>
2021-08-02 22:25:32 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-pub fn main() {
-    println!("Hello, world!");
+fn main() {
+    fizzbuzz_to(100);
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    if rhs == 0 {
+        return false;
+    }
+
+    lhs % rhs == 0
+}
+
+fn fizzbuzz(n: u32) -> () {
+    if is_divisible_by(n, 15) {
+        println!("fizzbuzz");
+    } else if is_divisible_by(n, 3) {
+        println!("fizz");
+    } else if is_divisible_by(n, 5) {
+        println!("buzz");
+    } else {
+        println!("{}", n);
+    }
+}
+
+fn fizzbuzz_to(n: u32) {
+    for n in 1..n + 1 {
+        fizzbuzz(n);
+    }
+}
+
