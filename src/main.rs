a7cd8d0fe4242ab40f25ef44f907ed79e127ffa8
alpha2phi <alpha2phi@gmail.com>
2021-08-22 12:06:32 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,55 @@
 fn main() {
-    fizzbuzz_to(100);
+    var_decl();
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+
+fn var_decl(){
+    let x ;
+    x = 42;
+
+    let x = 42;
+
+    let x: i32;
+
+    let x: i32 = 42;
+
+    let x;
+    // println!("{0}", x);
+    x = 42;
 }
 
-fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
-    if rhs == 0 {
-        return false;
-    }
 
-    lhs % rhs == 0
-}
+// fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+//     if rhs == 0 {
+//         return false;
+//     }
 
-fn fizzbuzz(n: u32) -> () {
-    if is_divisible_by(n, 15) {
-        println!("fizzbuzz");
-    } else if is_divisible_by(n, 3) {
-        println!("fizz");
-    } else if is_divisible_by(n, 5) {
-        println!("buzz");
-    } else {
-        println!("{}", n);
-    }
-}
+//     lhs % rhs == 0
+// }
 
-fn fizzbuzz_to(n: u32) {
-    for n in 1..n + 1 {
-        fizzbuzz(n);
-    }
-}
+// fn fizzbuzz(n: u32) -> () {
+//     if is_divisible_by(n, 15) {
+//         println!("fizzbuzz");
+//     } else if is_divisible_by(n, 3) {
+//         println!("fizz");
+//     } else if is_divisible_by(n, 5) {
+//         println!("buzz");
+//     } else {
+//         println!("{}", n);
+//     }
+// }
+
+// fn fizzbuzz_to(n: u32) {
+//     for n in 1..n + 1 {
+//         fizzbuzz(n);
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
 
